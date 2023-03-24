4892a67bf0a2f78999d44f250491c1fbf9983b1b
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-24 19:58:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 Update start.sh 
 
-if [ -z $UPSTREAM_REPO ]then echo "Cloning main Repository" git clone https://github.com/Devil-Botz/Elsa.git /Elsaelse echo "Cloning Custom Repo from $UPSTREAM_REPO " git clone $UPSTREAM_REPO /Elsaficd /Elsapip3 install -U -r requirements.txtecho "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"python3 bot.py
+if [ -z $UPSTREAM_REPO ]then echo "Cloning main Repository" git clone https://github.com/rajam0723/stylish-bot.git /Elsaelse echo "Cloning Custom Repo from $UPSTREAM_REPO " git clone $UPSTREAM_REPO /Elsaficd /Elsapip3 install -U -r requirements.txtecho "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"python3 bot.py
