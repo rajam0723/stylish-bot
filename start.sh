84a939d7a63f19cbce6c1b03188de8b058aa043b
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-24 21:32:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,20 +4,20 @@ then
 
   echo "Cloning main Repository"
 
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/rajam0723/stylish-bot.git /stylish-bot
 
 else
 
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
 
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /stylish-bot
 
 fi
 
-cd /PiroAutoFilterBot
+cd /stylish-bot
 
 pip3 install -U -r requirements.txt
 
-echo "Starting PiroAutoFilterBot...."
+echo "Starting stylish-bot...."
 
 python3 bot.py
