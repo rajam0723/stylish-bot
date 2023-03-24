9d4d49c30b7a49cf07d9895553400008bd2d389e
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-24 17:35:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/rajam0723/stylish-bot.git /stylish-bot
+  git clone rajam0723/stylish-bot.git /stylish-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /stylish-bot
 fi
-cd /Elsa
+cd /stylish-bot
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝐃𝐮𝐝𝐮....🧞‍♂️"
 python3 bot.py
